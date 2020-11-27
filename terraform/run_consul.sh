e378f09688b8f0b2dbc2bef119b3b9e85d16ca66
liathoffman <liatshoffman@gmail.com>
2020-11-28 01:19:22 +0200

    got the UI accessible from external b adding client flag

--- terraform/run_consul.sh
+++ terraform/run_consul.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo -i
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-consul agent -ui -data-dir=/tmp/consul -server -bootstrap-expect=3 -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1" 
+consul agent -datacenter=liatsdatacenter -client 0.0.0.0 -ui -data-dir=/tmp/consul -server -bootstrap-expect=3 -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1" 
