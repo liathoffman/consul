be980beed6b430eed2ba1513199042630f17b6a4
liathoffman <liatshoffman@gmail.com>
2020-11-28 01:05:59 +0200

    it was a security group issue - now all 3 servers are bootstrapping but UI is still not working

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,23 +48,13 @@ resource "aws_security_group" "instance-sg" {
   name   = "instance-sg"
   vpc_id = aws_default_vpc.default.id
 
-  # SSH access from anywhere
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP access from the anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
--- terraform/run_consul.sh
+++ terraform/run_consul.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo consul agent -data-dir=/tmp/consul -server -datacenter=liatdatacenter -bootstrap-expect=3 -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1" -ui
+sudo -i
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+consul agent -ui -data-dir=/tmp/consul -server -bootstrap-expect=3 -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1" 
