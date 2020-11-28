81228f74d4a742f8a9ae26da129a13505ea33122
liathoffman <liatshoffman@gmail.com>
2020-11-28 20:59:49 +0200

    fixed netowrking issue, can now ssh into instances again

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,22 @@ resource "aws_security_group" "instance-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
