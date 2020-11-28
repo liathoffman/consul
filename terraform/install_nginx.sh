0a9ffafd05e83a0028e1ee99d89ade80de3d7087
liathoffman <liatshoffman@gmail.com>
2020-11-28 19:17:39 +0200

    webserver is up and running, testing to ensure compiance with homework requirements

--- terraform/install_nginx.sh
+++ terraform/install_nginx.sh
@@ -6,4 +6,27 @@ apt-get install -y nginx
 systemctl start nginx
 systemctl enable nginx
 echo "This is server with hostname $HOSTNAME" | sudo tee /var/www/html/index.html
-consul agent -datacenter=liatsdatacenter -client 0.0.0.0 -ui -data-dir=/tmp/consul -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1"
+mkdir ./consul.d
+
+cat <<EOF > /consul.d/webserver.json
+{
+   "service":{
+      "name":"webserver",
+      "port":80,
+      "tags":[
+         "nginx"
+      ],
+      "checks":[
+         {
+            "id":"http",
+            "name":"Healthcheck for HTTP",
+            "http":"http://localhost:80/",
+            "interval":"5s",
+            "timeout":"1s"
+         }
+      ]
+   }
+}
+EOF
+
+consul agent -enable-script-checks -config-dir=./consul.d -datacenter=liatsdatacenter -client 0.0.0.0 -ui -data-dir=/tmp/consul -retry-join "provider=aws tag_key=Name tag_value=consul region=us-east-1"
